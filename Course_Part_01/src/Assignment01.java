daca1f4ad013dea1c450e6bfc7d9e6f0d31995ce
sjackjansma <sjackjansma@gmail.com>
2021-09-29 11:46:38 +0200

    SJA-0: initial

---
+++ Course_Part_01/src/Assignment01.java
@@ -0,0 +1,79 @@
+public class Assignment01 {
+
+    public static void main(String[] args) {
+        /*
+        * int
+        * min -2,147,483,648
+        * max 2,147,483,648
+        * default 0
+         */
+        int age=62;
+
+        /*
+         * boolean
+         * true or false
+         * default false
+         */
+        boolean isBirthdayToday = false;
+
+        /*
+         * byte
+         * min -128
+         * max 127
+         * default 0
+         */
+        byte byteVariable = 100;
+
+        /*
+         * short
+         * min -32,768
+         * max 32,768
+         * default 0
+         */
+        short shortVariable = 123;
+
+        /*
+         * long
+         * min -2^63
+         * max 2^63-1
+         * default 0
+         */
+        long longVariable = 2413132123L;
+
+        /*
+         * float
+         * min 32-bit IEEE
+         * max 754 floating-point
+         * default 0.0
+         */
+        float floatVariable = 1234.1234f;
+
+        /*
+         * double
+         * min 64-bit IEEE
+         * max 754 floating point
+         * default 0.0
+         */
+        double doubleVariable = 120.303;
+
+        /*
+         * char
+         * min '\u0000',' \uffff'
+         * max 0,65535
+         * default ' u0000'
+         */
+        char charVariable = 'a';
+
+        System.out.println ("Int value is " + age);
+        System.out.println ("Boolean value is " + isBirthdayToday);
+        System.out.println ("Byte value is " + byteVariable);
+        System.out.println ("Short value is " + shortVariable);
+        System.out.println ("Long value is " + longVariable);
+        System.out.println ("Float value is " + floatVariable);
+        System.out.println ("Double value is " + doubleVariable);
+        System.out.println ("Char value is " + charVariable);
+
+    }
+
+
+}
